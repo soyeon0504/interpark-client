c8a593c94ab537ddef698daf96d52d783bc501ef
soyeon0504 <dev.vsoyeon@gmail.com>
2023-10-20 17:21:41 +0900

    feat : 프로젝트 파일 정리 및 최적화

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div class="wrap">
+        <Header />
+        <div class="main">메인</div>
+        <Footer />
+    </div> 
+  );
+};
