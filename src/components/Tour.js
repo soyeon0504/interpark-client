fc0acfb26ad3123792addde0130d136a18dae128
soyeon0504 <dev.vsoyeon@gmail.com>
2023-11-09 11:37:53 +0900

    feat : Tour 리액트 완료

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,10 +1,18 @@
+
+import { BtCate } from "../components/ui/buttons";
 import { Swiper, SwiperSlide } from "swiper/react"
 
 import { Navigation } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/navigation";
+
 import "../styles/tour.css";
 import "../styles/common.css";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import styled from "@emotion/styled";
+import { InnerArea, SectionTag } from "./layout/layout";
+
 function Tour(){
     //js 코드 자리
     //JSX 의 요소를 React 에서 참조
@@ -17,51 +25,54 @@ function Tour(){
         .get("tour.json")
         .then(function(res){
             console.log(res.data);
+
+            const result = res.data;    
             let arr = [];
-        for(let i = 0; i < res.data.total; i++) {
-            const obj = res.data["tour_" + (i+ i)];
-            arr[i] = obj;
-        }    
-        console.log(arr);
-        setHtmlTag(arr);
+            for(let i = 0; i < result.total; i++) {
+                const obj = res.data["tour_" + (i+ 1)];
+                
+                arr[i] = obj;
+            }    
+            console.log(arr);
+            setHtmlTag(arr);
         })
         .catch(function(error){
             console.log(error)
         });
     }
-    // fetch("tour.json")
+    // axios ("tour.json")
     useEffect(()=> {
         axiosJsonData();
     }, [])
     return (
-        <section class="tour">
-                <div class="tour-inner">
-                    <div class="tour-header">
-                        <h2 class="tour-title">투어 특가</h2>
-                        <span class="tour-txt">해외여행은 인터파크다</span>
+        <SectionTag pt={0} pb={90}>
+                <InnerArea>
+                    <div className="tour-header">
+                        <h2 className="tour-title">투어 특가</h2>
+                        <span className="tour-txt">해외여행은 인터파크다</span>
                     </div>
-                    <div class="tour-main">
-                        <div class="tour-category">
-                            <ul class="tour-list">
+                    <div className="tour-main">
+                        <div className="tour-category">
+                            <ul className="tour-list">
                                 <li>
-                                    <button class="tour-cate-bt tour-cate-bt-active">망설이면 품절</button>
+                                    <BtCate active={true}>망설이면 품절</BtCate>
                                 </li>
                                 <li>
-                                    <button class="tour-cate-bt">패키지</button>
+                                    <BtCate>패키지</BtCate>
                                 </li>
                                 <li>
-                                    <button class="tour-cate-bt">국내숙소</button>
+                                    <BtCate>국내숙소</BtCate>
                                 </li>
                                 <li>
-                                    <button class="tour-cate-bt">해외숙소</button>
+                                    <BtCate>해외숙소</BtCate>
                                 </li>
                             </ul>
                         </div>
-                        <div class="tour-slide-wrap">
+                        <div className="tour-slide-wrap">
                         {/* <!-- Swiper --> */}
                         <Swiper 
                     slidesPerView={3} 
-                    spaceBetween={26} 
+                    spaceBetween={28} 
                     slidesPerGroup={3} 
                     onSwiper={(swiper)=>{
                         swiperRef.current= swiper;
@@ -73,32 +84,32 @@ function Tour(){
                     }}
                     className="tour-slide">
 
-                    {
-                      htmlTag.map((item, index) => {
+                    {htmlTag.map((item, index) => {
                         return(
                           <SwiperSlide key={index}>
-                            {
-                              (index === htmlTag.length - 1) ? (<a href={item.url}>바로가기</a>) : (
-                                <div class="swiper-slide">
-                                <div class="tour-slide-item">
-                                  <a href={item.url} class="tour-link">
-                                    <div class="tour-img">
+                            {index === htmlTag.length - 1 ? (
+                                <a href={item.url}>바로가기</a>
+                                ) : (
+                                <div className="swiper-slide">
+                                    <div className="tour-slide-item">
+                                  <a href={item.url} className="tour-link">
+                                    <div className="tour-img">
                                       <img src={item.image} alt={item.desc} />
                                     </div>
-                                    <div class="tour-state">${item.state}</div>
-                                    <div class="tour-info">
-                                      <ul class="tour-good-list">
+                                    <div className="tour-state">{item.state}</div>
+                                    <div className="tour-info">
+                                      <ul className="tour-good-list">
                                         <li>
-                                            <span class="tour-good-info-title">
-                                                <div>${item.title}</div>
+                                            <span className="tour-good-info-title">
+                                                <div>{item.title}</div>
                                             </span>
-                                            <p class="tour-good-info-desc">
-                                            ${item.desc}
+                                            <p className="tour-good-info-desc">
+                                            {item.desc}
                                             </p>
                                         </li>
                                         <li>
-                                          <span class="tour-good-info-price">
-                                          <em>${item.price}</em>
+                                          <span className="tour-good-info-price">
+                                          <em>{item.price}</em>
                                           원~
                                           </span>
                                         </li>
@@ -111,28 +122,29 @@ function Tour(){
                             }
                             
                           </SwiperSlide>
-                        )
-                      })
-                    }
+                        );
+                      })}
                     </Swiper>
-                        <button class="slide-prev-bt">
+
+                        <button className="slide-prev-bt">
                             <img src="images/slider_arrow.svg" alt=""/>
                         </button>
-                        <button class="slide-next-bt">
+                        <button className="slide-next-bt">
                             <img src="images/slider_arrow.svg" alt=""/>
                         </button>
                          {/* <!-- //투어 특가 슬라이드 --> */}
                         </div>
                     </div>
-                </div>
-                <div>
-                    <div class="tour-footer">
-                        <button class="tour-footer-bt">투어 홈 바로가기
+
+                    <div>
+                    <div className="tour-footer">
+                        <button className="tour-footer-bt">투어 홈 바로가기
                             <img src="images/icon_arrow.svg" alt=""/>
                         </button>
                     </div>
                 </div>
-    </section>
+                </InnerArea>
+    </SectionTag>
     )
 }
 
